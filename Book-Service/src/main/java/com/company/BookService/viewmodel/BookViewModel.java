0ab8fa6253d092644f362574ea8f3353b1aeee21
anicho91 <a.k.nichols@live.com>
2019-10-02 12:48:09 -0400

    added jackson dependencies

--- Book-Service/src/main/java/com/company/BookService/viewmodel/BookViewModel.java
+++ Book-Service/src/main/java/com/company/BookService/viewmodel/BookViewModel.java
@@ -1,4 +1,44 @@
 package com.company.BookService.viewmodel;
 
+import java.util.List;
+
 public class BookViewModel {
+
+    private int id;
+    private String title;
+    private String author;
+    private List<?> notes;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public List<?> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<?> notes) {
+        this.notes = notes;
+    }
+
 }
