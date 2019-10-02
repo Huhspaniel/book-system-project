78076fe9fa3b3c1a293cd30d0803b91e4d21730a
Jordan Senken <jsenken@yahoo.com>
2019-10-02 15:43:00 -0400

    note service

---
+++ note-service/src/main/java/com/company/noteservice/model/Note.java
@@ -0,0 +1,61 @@
+package com.company.noteservice.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name="note")
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int noteId;
+
+    private int bookId;
+
+    @Size(max = 255, message = "Max char 255")
+    private String note;
+
+    public int getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(int noteId) {
+        this.noteId = noteId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note){
+        this.note = note;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note1 = (Note) o;
+        return noteId == note1.noteId &&
+                bookId == note1.bookId &&
+                note.equals(note1.note);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(noteId, bookId, note);
+    }
+
+}
