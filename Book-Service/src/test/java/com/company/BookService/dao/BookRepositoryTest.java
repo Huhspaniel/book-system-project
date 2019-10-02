9c04739d08036e671ccb7fd87a9e692592f3e0a3
anicho91 <a.k.nichols@live.com>
2019-10-02 13:59:04 -0400

    finished dao tests

--- Book-Service/src/test/java/com/company/BookService/dao/BookRepositoryTest.java
+++ Book-Service/src/test/java/com/company/BookService/dao/BookRepositoryTest.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
@@ -43,28 +45,70 @@ public class BookRepositoryTest {
         book.setTitle("Book Title");
         book.setAuthor("Author Name");
 
-        bookRepo.save(book);
-
         Book book2 = new Book();
-        book2.setTitle("Book Title");
-        book2.setAuthor("Author Name");
+        book2.setTitle("Book Title2");
+        book2.setAuthor("Author Name2");
 
         bookRepo.save(book);
+        bookRepo.save(book2);
+
+        Book getBook = bookRepo.getOne(book.getId());
+        assertEquals("Author Name", book.getAuthor());
 
     }
 
     @Test
     public void getAllBookTest(){
 
+        Book book = new Book();
+        book.setTitle("Book Title");
+        book.setAuthor("Author Name");
+
+        Book book2 = new Book();
+        book2.setTitle("Book Title2");
+        book2.setAuthor("Author Name2");
+
+        bookRepo.save(book);
+        bookRepo.save(book2);
+
+        List<Book> books = bookRepo.findAll();
+        assertEquals(2, books.size());
+
     }
 
     @Test
     public void deleteBookTest(){
 
+        Book book = new Book();
+        book.setTitle("Book Title");
+        book.setAuthor("Author Name");
+
+        Book book2 = new Book();
+        book2.setTitle("Book Title2");
+        book2.setAuthor("Author Name2");
+
+        bookRepo.save(book);
+        bookRepo.save(book2);
+
+        bookRepo.deleteById(book.getId());
+        List<Book> books = bookRepo.findAll();
+
+        assertEquals(1, books.size());
     }
 
     @Test
     public void updateBookTest(){
+        Book book = new Book();
+        book.setTitle("Book Title");
+        book.setAuthor("Author Name");
+        bookRepo.save(book);
+
+        book.setAuthor("Author Name2");
+        bookRepo.save(book);
+
+        Book getBook = bookRepo.getOne(book.getId());
+        assertEquals("Author Name2", book.getAuthor());
+
 
     }
 
---
+++ Book-Service/src/test/java/com/company/BookService/service/BookServiceLayerTest.java
@@ -0,0 +1,12 @@
+package com.company.BookService.service;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class BookServiceLayerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
